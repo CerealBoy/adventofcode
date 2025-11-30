8a54a6107786526c1f996cc0cb2b9ed07e20771f
CerealBoy <allan@buildkite.com>
2025-12-01 10:28:40 +1100

    Adding the skeleton for 2025 🩻

--- main.go
+++ main.go
@@ -12,26 +12,26 @@ import (
 	"github.com/CerealBoy/adventofcode/year2022"
 	"github.com/CerealBoy/adventofcode/year2023"
 	"github.com/CerealBoy/adventofcode/year2024"
+	"github.com/CerealBoy/adventofcode/year2025"
 	"github.com/alecthomas/kong"
 )
 
-var (
-	cli struct {
-		Year2015 year2015.Cmd `cmd help:"Year 2015 for the question."`
-		Year2016 year2016.Cmd `cmd help:"Year 2016 for the question."`
-		Year2017 year2017.Cmd `cmd help:"Year 2017 for the question."`
-		Year2018 year2018.Cmd `cmd help:"Year 2018 for the question."`
-		Year2019 year2019.Cmd `cmd help:"Year 2019 for the question."`
-		Year2020 year2020.Cmd `cmd help:"Year 2020 for the question."`
-		Year2021 year2021.Cmd `cmd help:"Year 2021 for the question."`
-		Year2022 year2022.Cmd `cmd help:"Year 2022 for the question."`
-		Year2023 year2023.Cmd `cmd help:"Year 2023 for the question."`
-		Year2024 year2024.Cmd `cmd help:"Year 2024 for the question."`
+var cli struct {
+	Year2015 year2015.Cmd `cmd help:"Year 2015 for the question."`
+	Year2016 year2016.Cmd `cmd help:"Year 2016 for the question."`
+	Year2017 year2017.Cmd `cmd help:"Year 2017 for the question."`
+	Year2018 year2018.Cmd `cmd help:"Year 2018 for the question."`
+	Year2019 year2019.Cmd `cmd help:"Year 2019 for the question."`
+	Year2020 year2020.Cmd `cmd help:"Year 2020 for the question."`
+	Year2021 year2021.Cmd `cmd help:"Year 2021 for the question."`
+	Year2022 year2022.Cmd `cmd help:"Year 2022 for the question."`
+	Year2023 year2023.Cmd `cmd help:"Year 2023 for the question."`
+	Year2024 year2024.Cmd `cmd help:"Year 2024 for the question."`
+	Year2025 year2025.Cmd `cmd help:"Year 2025 for the question."`
 
-		Debug bool `help:"Enable debug mode." short:"d"`
-		Test  bool `help:"Whether the test should be used." short:"t"`
-	}
-)
+	Debug bool `help:"Enable debug mode." short:"d"`
+	Test  bool `help:"Whether the test should be used." short:"t"`
+}
 
 func main() {
 	ctx := kong.Parse(&cli)
---
+++ year2025/cmd.go
@@ -0,0 +1,30 @@
+// Package year2025 contains the day based solutions for the 2025
+// run of Advent of Code.
+package year2025
+
+import (
+	"errors"
+
+	"github.com/CerealBoy/adventofcode/shared"
+)
+
+var (
+	days  = map[int]func(*shared.Context) error{}
+	debug = false
+)
+
+// Cmd defines the current year as a command, with any arguments.
+type Cmd struct {
+	Day int `help:"Day of the year to run."`
+}
+
+// Run will accept the context from the command and look to run the
+// given day with it, returning an error from the execution.
+func (c *Cmd) Run(ctx *shared.Context) error {
+	f, ok := days[c.Day]
+	if !ok {
+		return errors.New(shared.InvalidDay)
+	}
+
+	return f(ctx)
+}
